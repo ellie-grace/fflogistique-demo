88a30eac8bc3eef697e2aa8e01ca328af712b042
Ellie Grace <info@builtbyellie.com>
2019-01-28 17:34:01 +0100

    first commit

---
+++ public/scripts/main.js
@@ -0,0 +1,71 @@
+
+// email encrypting
+//STEPHANE
+var stParts=["stephane","fflogistique","fr","&#46;","&#64;"];
+var stEmail=stParts[0]+stParts[4]+stParts[1]+stParts[3]+stParts[2];
+var emailst=document.querySelector('#email-st');
+emailst.innerHTML=stEmail;
+
+// romuald
+
+var roParts=["romuald","fflogistique","co.uk","&#46;","&#64;"];
+var roEmail=roParts[0]+roParts[4]+roParts[1]+roParts[3]+roParts[2];
+var emailro=document.querySelector('#email-ro');
+emailro.innerHTML=roEmail;
+
+// karolina
+
+var kaParts=["karolina","fflogistique","fr","&#46;","&#64;"];
+var kaEmail=kaParts[0]+kaParts[4]+kaParts[1]+kaParts[3]+kaParts[2];
+var emailka=document.querySelector('#email-ka');
+emailka.innerHTML=kaEmail;
+
+// paula
+
+var paParts=["paula","fflogistique","fr","&#46;","&#64;"];
+var paEmail=paParts[0]+paParts[4]+paParts[1]+paParts[3]+paParts[2];
+var emailpa=document.querySelector('#email-pa');
+emailpa.innerHTML=paEmail;
+
+// arnaud
+
+var arParts=["arnaud","fflogistique","fr","&#46;","&#64;"];
+var arEmail=arParts[0]+arParts[4]+arParts[1]+arParts[3]+arParts[2];
+var emailar=document.querySelector('#email-ar');
+emailar.innerHTML=arEmail;
+
+// michael
+
+var miParts=["michael","fflogistique","fr","&#46;","&#64;"];
+var miEmail=miParts[0]+miParts[4]+miParts[1]+miParts[3]+miParts[2];
+var emailmi=document.querySelector('#email-mi');
+emailmi.innerHTML=miEmail;
+
+// martin rungis
+
+var ruParts=["rungis","fflogistique","fr","&#46;","&#64;"];
+var ruEmail=ruParts[0]+ruParts[4]+ruParts[1]+ruParts[3]+ruParts[2];
+var emailru=document.querySelector('#email-ru');
+emailru.innerHTML=ruEmail;
+
+// patrick
+
+var patParts=["patrick","fflogistique","fr","&#46;","&#64;"];
+var patEmail=patParts[0]+patParts[4]+patParts[1]+patParts[3]+patParts[2];
+var emailpat=document.querySelector('#email-pat');
+emailpat.innerHTML=patEmail;
+
+
+// valerie
+
+var vaParts=["valerie","fflogistique","fr","&#46;","&#64;"];
+var vaEmail=vaParts[0]+vaParts[4]+vaParts[1]+vaParts[3]+vaParts[2];
+var emailva=document.querySelector('#email-va');
+emailva.innerHTML=vaEmail;
+
+
+// dominique
+var doParts=["dominique","fflogistique","fr","&#46;","&#64;"];
+var doEmail=doParts[0]+doParts[4]+doParts[1]+doParts[3]+doParts[2];
+var emaildo=document.querySelector('#email-do');
+emaildo.innerHTML=doEmail;
